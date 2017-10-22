6b3f4b241ed4ff352569f9866239fb7cc40bbb86
bliunh <bliu.nh@gmail.com>
2017-10-22 09:44:55 -0400

    initial

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //database: 'mongodb://admin:Bxlford5m@ds121190.mlab.com:21190/sampledb',   //prod
+  database: 'mongodb://localhost:27017/sampledb',    //dev
+  secret: 'sampledbsecret'
+}
